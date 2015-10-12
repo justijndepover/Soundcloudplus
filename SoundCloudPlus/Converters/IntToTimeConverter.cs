da1daf4b04f8405e1afdb51963e174805a80476b
Arn Vanhoutte <arnvanhoutte@hotmail.com>
2015-10-12 11:42:12 +0200

    Slightly better UI in MainPage

---
+++ SoundCloudPlus/Converters/IntToTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SoundCloudPlus.Converters
+{
+    public class IntToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int val = (int)value;
+            DateTime time = new DateTime();
+            time = time.AddMilliseconds(val);
+            return time.Minute + ":" + time.Second;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
