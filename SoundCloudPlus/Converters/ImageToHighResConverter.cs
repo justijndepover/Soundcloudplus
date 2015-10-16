c27153ab43f49387f3146c91f167e03554ef23bb
Arn Vanhoutte <arnvanhoutte@hotmail.com>
2015-10-16 11:27:14 +0200

    Added HighResConverter

---
+++ SoundCloudPlus/Converters/ImageToHighResConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SoundCloudPlus.Converters
+{
+    public class ImageToHighResConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string source = (string) value;
+            return source.Replace("large.jpg", "t500x500.jpg");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
