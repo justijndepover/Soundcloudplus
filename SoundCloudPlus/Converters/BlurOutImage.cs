86a31fd8115762f2fb3dcb2a11d9c6d9517f8404
justijndepover <justijndepover@gmail.com>
2015-12-18 15:15:01 +0100

    Added support to blur images

---
+++ SoundCloudPlus/Converters/BlurOutImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+using ClassLibrary.Common;
+
+namespace SoundCloudPlus.Converters
+{
+    class BlurOutImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Image v = value as Image;
+            //BitmapImage b = await BlurImage.BlurOutImage(v);
+            return v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SoundCloudPlus/Pages/PlayingPage.xaml.cs
+++ SoundCloudPlus/Pages/PlayingPage.xaml.cs
@@ -1,19 +1,12 @@
-﻿using SoundCloudPlus.ViewModels;
+﻿using ClassLibrary.Common;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using SoundCloudPlus.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Core;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -28,7 +21,7 @@ namespace SoundCloudPlus.Pages
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             var currentView = SystemNavigationManager.GetForCurrentView();
@@ -42,6 +35,7 @@ namespace SoundCloudPlus.Pages
                 {
                     _mainPageViewModel = MainPage.Current._mainPageViewModel;
                     LayoutRoot.DataContext = _mainPageViewModel;
+                    blurImage.Source = await BlurImage.BlurOutImage(_mainPageViewModel.PlayingTrack.ArtworkUrl as Image);
                 }
                 catch (Exception ex)
                 {
@@ -64,5 +58,6 @@ namespace SoundCloudPlus.Pages
 
             currentView.BackRequested -= CurrentView_BackRequested;
         }
+        
     }
 }
